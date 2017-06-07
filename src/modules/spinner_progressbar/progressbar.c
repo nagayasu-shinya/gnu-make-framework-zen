36c26503549f772e863beff6cc756a2f3f44779d
NAGAYASU Shinya <nagayasu_shinya@yahoo.co.jp>
2017-06-07 14:39:13 +0900

    add comment, mit license.

--- src/modules/spinner_progressbar/progressbar.c
+++ src/modules/spinner_progressbar/progressbar.c
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 NAGAYASU Shinya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include "spinner_progressbar.h"
@@ -20,9 +44,9 @@ int sample_progressbar(unsigned int wait_sec)
 
         printf("\033[2K\033[G %d sec\n", i);
         printf("\033[2F");
-        fflush(stdout); // ÉoÉbÉtÉ@Çã≠êßìIÇ…èoóÕ.
+        fflush(stdout); // „Éê„ÉÉ„Éï„Ç°„ÇíÂº∑Âà∂ÁöÑ„Å´Âá∫Âäõ.
     }
 
-    puts(""); // â¸çsï\é¶.
+    puts(""); // ÊîπË°åË°®Á§∫.
     return 0;
 }
--- src/modules/spinner_progressbar/spinner.c
+++ src/modules/spinner_progressbar/spinner.c
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 NAGAYASU Shinya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include "spinner_progressbar.h"
@@ -11,9 +35,9 @@ int sample_spinner(unsigned int wait_sec)
     for (i = 0; i < wait_sec; i++) {
         (void) sleep(1);
         printf("\rwating %d sec... %c", wait_sec, spinner_c[i % spinner_len]);
-        fflush(stdout); // ƒoƒbƒtƒ@‚ð‹­§“I‚Éo—Í.
+        fflush(stdout); // ãƒãƒƒãƒ•ã‚¡ã‚’å¼·åˆ¶çš„ã«å‡ºåŠ›.
     }
 
-    puts(""); // ‰üs•\Ž¦.
+    puts(""); // æ”¹è¡Œè¡¨ç¤º.
     return 0;
 }
